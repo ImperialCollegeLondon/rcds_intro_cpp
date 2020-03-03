eee148ee105f49bc351cf4e5a36ec126ae6195b2
JesusUrtasun <Jesus.Urtasun@mi.infn.it>
2020-03-03 11:21:48 +0100

    Preparing Chapter8

---
+++ Chapter08/solutions/exercise2.cc
@@ -0,0 +1,30 @@
+/*
+  Exercise: Headers and source
+  Author: Jesús Urtasun - 2020
+*/
+
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+int main() {
+
+  // Declare variables
+  double a = 5, b = 2;
+
+  cout << "Initial values:" << endl;
+  cout << a << " " << b << endl;
+
+  // Call change1 function
+  cout << "First change with change1:" << endl;
+  change1(a,b);
+  cout << a << " " << b << endl;
+
+  // Call change2 function
+  cout << "Second change with change2:" << endl;
+  change2(&a, &b);
+  cout << a << " " << b << endl;
+  
+  return 0;
+
+}
---
+++ Chapter08/solutions/functions.cc
@@ -0,0 +1,16 @@
+// Implementation
+#include "functions.h"
+
+void change1(double &a, double &b)
+{
+  double dep = b;
+  b = a;
+  a = dep;
+}
+
+void change2(double *a, double *b)
+{
+  double dep = *b;
+  *b = *a;
+  *a = dep;
+}
