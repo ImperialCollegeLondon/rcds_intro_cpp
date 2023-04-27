ddb6f42f6d4b02fdface3351fa260115511cc6c7
jurtasun <jurtasun@ic.ac.uk>
2023-04-27 11:09:25 +0100

    Prepare chapter1

---
+++ Chapter1/solutions/helloworld.cpp
@@ -0,0 +1,16 @@
+/*
+  Exercise 1: Hello World
+  Print the "Hello World!" message on screen
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // Print on screen
+  cout << "Hello World!" << endl;
+
+  return 0;
+
+}
