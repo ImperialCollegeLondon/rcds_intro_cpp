cf923f56791d699407c22a2dc28ffafb8f1badec
Jesús Urtasun <jurtasun@MRC-12998.local>
2025-03-25 12:07:22 +0000

    Prepare chapter2

---
+++ Chapter2/exercises/increase.cpp
@@ -0,0 +1,27 @@
+/*
+  Exercise 1: Increasing / Decreasing
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // Declare variables
+
+  // Read the number as input
+
+  // Compute operations
+  int increasing, decreasing, division, rest;
+  
+  // Print on screen
+  cout << "number: " <<  << endl;
+  cout << "ratio: " <<  << endl;
+  cout << "increased: " <<  << endl;
+  cout << "decreased: " <<  << endl;
+  cout << "division : " <<  << endl;
+  cout << "rest     : " <<  << endl;  
+  
+  return 0;
+
+}
